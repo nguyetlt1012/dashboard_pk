091d955ddfa6cec52de1896d50c83d83e71c2163
Nguyet Le <nguyet.le@platform.inc>
2023-01-17 22:37:04 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+import { Breadcrumb, Layout, Menu, theme } from "antd";
+const { Header, Content, Footer } = Layout;
+const App = () => {
+	const {
+		token: { colorBgContainer },
+	} = theme.useToken();
+	return (
+		<Layout>
+			<Header style={{ position: "sticky", top: 0, zIndex: 1, width: "100%" }}>
+				<div
+					style={{
+						float: "left",
+						width: 120,
+						height: 31,
+						margin: "16px 24px 16px 0",
+						background: "rgba(255, 255, 255, 0.2)",
+					}}
+				/>
+				<Menu
+					theme="dark"
+					mode="horizontal"
+					defaultSelectedKeys={["2"]}
+					items={new Array(3).fill(null).map((_, index) => ({
+						key: String(index + 1),
+						label: `nav ${index + 1}`,
+					}))}
+				/>
+			</Header>
+		</Layout>
+	);
+};
 export default App;
